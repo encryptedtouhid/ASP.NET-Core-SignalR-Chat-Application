12248edb01d1e680e8041b2e510129856fa56f28
Khaled Md Tuhidul Hossain <mth.tuhin@gmail.com>
2020-04-12 20:13:03 +0600

    added

--- ChatAPI/ChatHub.cs
+++ ChatAPI/ChatHub.cs
@@ -26,34 +26,22 @@ namespace ChatAPI
             await base.OnDisconnectedAsync(exception);
         }
 
-        public async Task SendMessage(Guid senderID, Guid recipientID, string contents)
+        public async Task SendMessage(Message message)
         {
             Message newMessage = new Message()
             {
-                SenderUniqueCode = senderID,
-                RecipientUniqueCode = recipientID,
-                Contents = contents,
+                SenderUniqueCode = message.SenderUniqueCode,
+                RecipientUniqueCode = message.RecipientUniqueCode,
+                Contents = message.Contents,
                 Timestamp = DateTime.UtcNow
             };
             _dbContext.Message.Add(newMessage);
             await _dbContext.SaveChangesAsync();
 
 
-            await Clients.Users(senderID.ToString(), newMessage.RecipientUniqueCode.ToString()).SendAsync("ReceiveMessage", newMessage);
+            await Clients.Users(message.SenderUniqueCode.ToString(), newMessage.RecipientUniqueCode.ToString()).SendAsync("ReceiveMessage", newMessage);
         }
 
-        //public async Task GetUserOnlineStatus(string UserID)
-        //{
-        //    int userID = 0;
-        //    if (int.TryParse(UserID, out userID) == false)
-        //        throw new InvalidCastException("UserID should be a number");
-
-        //    DateTime lastActive = await _statusMonitorService.GetUserStatus(userID);
-        //    if (lastActive == new DateTime(0))
-        //        await Clients.Caller.SendAsync("ReceiveUserStatus", 0);
-        //    else
-        //        await Clients.Caller.SendAsync("ReceiveUserStatus", lastActive);
-        //}
 
     }
 }
---
+++ ChatClient/Message.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatClient
+{
+	
+/// <summary>
+	/// 
+	/// </summary>
+	public class Message
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		public int MessageId { get; set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public int UserChatId { get; set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public int EventId { get; set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public string recipientID { get; set; }
+		public string senderID { get; set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public string contents { get; set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public DateTime CreatedDate { get; set; }
+
+	}
+}
